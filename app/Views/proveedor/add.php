cb4b07dd16a3315faa80db11d6c40b19881ff34a
emmanuel <emmvazquez@gmail.com>
2024-08-28 13:53:02 -0600

    formulario add proveedor

--- app/Views/proveedor/add.php
+++ app/Views/proveedor/add.php
@@ -1,12 +1,32 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <select name="idEstado">
+            <h2>Agregar proveedor</h2>
+
+            <label for="nombre">Nombre</label>
+            <input type="text" class="form-control" name="nombre">
+
+            <label for="telefono">Teléfono</label>
+            <input type="text" class="form-control" name="telefono">
+
+            <label for="calle">Calle</label>
+            <input type="text" class="form-control" name="calle">
+
+            <label for="numero">Número</label>
+            <input type="text" class="form-control" name="numero">
+
+            <label for="CP">CP</label>
+            <input type="text" class="form-control" name="cp">
+
+            <label for="idEstado">Estado</label>
+            <select name="idEstado" class="form-control">
                 <?php foreach ($estados as $key ) :?>
                    <option value="<?=$key->idEstado?>"><?=$key->estado?></option>
                 <?php endforeach ?>
 
             </select>
+            
+            <input type="submit" class="btn btn-success mt-3" >
         </div>
     </div>
 </div>
